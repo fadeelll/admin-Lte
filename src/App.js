3eaaa3dff9bc81bb0bada9579631693872496a30
fadeelll <fadeelmubarokwok08@gmail.com>
2024-11-24 19:15:04 +0700

    fadeelll

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Home from './components/Home'
+import Sidenav from './components/Sidenav'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Home />
+    <Sidenav />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
